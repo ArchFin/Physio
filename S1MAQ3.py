eea903a7835ff4285429669199b649ff736c3318
Archie Finney <archie.finney@yahoo.co.uk>
2024-09-14 16:23:42 +0100

    small update

--- S1MAQ3.py
+++ S1MAQ3.py
@@ -6,30 +6,34 @@ from scipy.stats import linregress
 
 
 #Solution to part (iii)
-def sn(N, b, c, sb):
-    return b**(N)*sb + ((b**(N)-1)/(b-1))*c
+def sn(N, c, sb, a):
+    
+    for j in a:
+        b = (j+1)/2
+        s_n = []
+        for i in N:
+            y = b**(i)*sb + ((b**(i)-1)/(b-1))*c
+            s_n.append(y)
+        fig, axs = plt.subplots(1)
+        fig.set_size_inches(180/25.4,50/25.4)
+        axs.set_xlabel('$N$')
+        axs.set_ylabel('s_n')
+        axs.plot(N,s_n)
+
 
 #getting number values
-N = np.geomspace(1e0,1e2,1) # geometric spacing
-b = 2 
+N = range(0,10,1) # geometric spacing
+a = np.arange(-1,1,0.1)
 c = 0.3
 sb = 1
 
-s_n = sn(N, b, c, sb)
+s_n = sn(N, c, sb, a)
 
 #setting up the figure for visualisation
-fig, axs = plt.subplots(1, 2)
-fig.set_size_inches(180/25.4,50/25.4)
-ax = axs[0]
-ax.set_xscale('log')
-ax.set_xlabel('$N$')
-ax.set_ylabel('dN/dt')
-ax.plot(N,s_n)
-ax = axs[1]
-ax.set_xscale('log')
-ax.set_xlabel('$N$')
-ax.set_yscale('log')
-ax.set_ylabel('|dN/dt|')
-ax.plot(N,np.abs(s_n))
-fig.tight_layout()
+# fig, axs = plt.subplots(1)
+# fig.set_size_inches(180/25.4,50/25.4)
+# axs.set_xlabel('$N$')
+# axs.set_ylabel('s_n')
+# axs.plot(N,s_n)
+
 plt.show()
